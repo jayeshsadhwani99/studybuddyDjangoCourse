f104ecf58f905b7e9d62d72040febe0f6b8098e0
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2022-01-27 14:54:31 +0530

    Conect views to db

--- base/views.py
+++ base/views.py
@@ -1,29 +1,12 @@
 from django.shortcuts import render
-
-rooms = [
-    {
-        'id': 1,
-        'name': "Room 1",
-    },
-    {
-        'id': 2,
-        'name': "Room 2",
-    },
-    {
-        'id': 3,
-        'name': "Room 3",
-    },
-]
+from .models import Room
 
 def home(request):
+    rooms = Room.objects.all()
     return render(request, 'base/home.html', {'rooms': rooms})
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if(i['id'] == int(pk)):
-            room = i
-            break
+    room = Room.objects.get(id=pk)
     context = {'room': room}
     return render(request, 'base/room.html', context)
 
