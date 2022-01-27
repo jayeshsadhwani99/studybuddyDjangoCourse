1ac6939c2d8244dac5a79158ae8eccbec65fb8aa
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2022-01-27 14:21:42 +0530

    Jinja

--- base/views.py
+++ base/views.py
@@ -1,9 +1,23 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
+rooms = [
+    {
+        'id': 1,
+        'name': "Room 1",
+    },
+    {
+        'id': 2,
+        'name': "Room 2",
+    },
+    {
+        'id': 3,
+        'name': "Room 3",
+    },
+]
+
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'base/home.html', {'rooms': rooms})
 
 def room(request):
-    return render(request, 'room.html')
+    return render(request, 'base/room.html')
 
