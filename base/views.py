1babefede542d33fa776fb56dd9bcd7a5c04cf15
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2022-01-28 23:26:45 +0530

    Search

--- base/views.py
+++ base/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import redirect, render
-from .models import Room
+from .models import Room, Topic
 from .forms import RoomForm
+from django.db.models import Q
 
 def home(request):
-    rooms = Room.objects.all()
-    return render(request, 'base/home.html', {'rooms': rooms})
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+
+    rooms = Room.objects.filter(Q(topic__name__icontains=q)|Q(name__icontains=q)|Q(description__icontains=q))
+    topics = Topic.objects.all()
+
+    room_count = rooms.count()
+    context = {'rooms': rooms, 'topic': topics, 'room_count': room_count}
+    return render(request, 'base/home.html', context)
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
