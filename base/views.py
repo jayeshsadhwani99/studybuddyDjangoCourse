ea74a3da3778413af6ab9a734caf6a746684f319
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2022-02-25 18:02:43 +0530

    Topics

--- base/views.py
+++ base/views.py
@@ -59,7 +59,7 @@ def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
 
     rooms = Room.objects.filter(Q(topic__name__icontains=q)|Q(name__icontains=q)|Q(description__icontains=q))
-    topics = Topic.objects.all()
+    topics = Topic.objects.all()[0:5]
     room_count = rooms.count()
     room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
 
@@ -153,3 +153,8 @@ def updateUser(request):
             return redirect('user-profile', pk=user.id)
     
     return render(request, 'base/update_user.html', {'form': form})
+
+def topicsPage(request):
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    topics = Topic.object.filter(name__icontains=q)
+    return render(request, 'base/topics.html', {'topics': topics})
