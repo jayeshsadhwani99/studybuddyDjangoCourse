70c2d5886796d76216b2fd6023120a79afa89c34
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2022-02-25 18:07:50 +0530

    object->objects

--- base/views.py
+++ base/views.py
@@ -156,9 +156,9 @@ def updateUser(request):
 
 def topicsPage(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
-    topics = Topic.object.filter(name__icontains=q)
+    topics = Topic.objects.filter(name__icontains=q)
     return render(request, 'base/topics.html', {'topics': topics})
 
 def activityPage(request):
-    room_messages = Message.object.all()
+    room_messages = Message.objects.all()
     return render(request, 'base/activity.html', {'room_messages': room_messages})
